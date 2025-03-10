e316618da604dce42e6e3d0a1b6c4ffb8472c7c5
ServicedepubliciteVVW <veronik.valeria.william@gmail.com>
2025-03-10 03:46:43 +0000

    Add files via upload

---
+++ javascripts/app.js
@@ -0,0 +1,70 @@
+;(function ($, window, undefined) {
+  'use strict';
+
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
+
+  
+  $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+  $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+  $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
+  $('input, textarea').placeholder();
+  
+  
+  $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+  
+  
+  $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+  
+  
+  $.fn.foundationTopBar           ? $doc.foundationTopBar({breakPoint: 940}) : null;
+  
+  
+  $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+  
+    
+    $.fn.foundationTabs             ? $doc.foundationTabs() : null;
+    
+  
+  
+    $("#featured").orbit();
+  
+
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
+
+  // Hide address bar on mobile devices
+  if (Modernizr.touch) {
+    $(window).load(function () {
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+
+})(jQuery, this);
+
+
+
+
+
+let index = 0;
+const slides = document.querySelectorAll(".carousel-item");
+
+function showSlide(n) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[n].classList.add("active");
+}
+
+function moveSlide(step) {
+    index += step;
+    if (index >= slides.length) index = 0;
+    if (index < 0) index = slides.length - 1;
+    showSlide(index);
+}
+
+// Cambia automáticamente cada 4 segundos
+setInterval(() => moveSlide(1), 4000);
